9c5196c42f19e9e9f53a7eea3f36b4bb90b8c887
leuhsouza <leuhsouza@gmail.com>
2024-03-11 06:49:43 -0300

    feat: private show

--- projeto-mvc/config/routes.rb
+++ projeto-mvc/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get 'products', to: "products#index"
+  get 'products/:id', to: "products#show"
 
   # Defines the root path route ("/")
   # root "posts#index"
