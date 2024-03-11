60da008af18f44c963058d007dba21b716fe673c
leuhsouza <leuhsouza@gmail.com>
2024-03-11 09:09:04 -0300

    wip: add form and save

--- projeto-mvc/app/controllers/products_controller.rb
+++ projeto-mvc/app/controllers/products_controller.rb
@@ -6,8 +6,26 @@ class ProductsController < ApplicationController
 
   def show; end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new{product_params}
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+
+    end
+  end
+
   private
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :quantity, :price)
   end
 end
--- projeto-mvc/config/routes.rb
+++ projeto-mvc/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get 'products', to: "products#index"
+  get 'products', to: "products#create"
+  get 'products/new', to: "products#new"
   get 'products/:id', to: "products#show"
 
   # Defines the root path route ("/")
